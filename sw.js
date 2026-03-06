7fa991faae14b50299f0b7a3b52ea92e9620ec2b
Edward Vikhr <emacron891@gmail.com>
2026-03-06 23:12:14 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+// sw.js (Service Worker for Push)
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, { body: data.body });
+});
